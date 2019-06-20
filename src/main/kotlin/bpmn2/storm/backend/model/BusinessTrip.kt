4c7d81f2257816dfbd38b81559dc894421345395
Denis Kotov <kotov@bpmn2.ru>
2019-06-20 19:13:05 +0300

    clear project

---
+++ src/main/kotlin/bpmn2/storm/backend/model/BusinessTrip.kt
@@ -0,0 +1,36 @@
+package bpmn2.storm.backend.model
+
+import java.io.Serializable
+import java.util.Date
+/*
+class BusinessTrip: Serializable {
+    val from: String? = null
+    val to: String? = null
+    val dateFrom: Date? = null
+    val dateTo: Date? = null
+    val reason: String? = null
+    val totalAmount: Double? = null
+    val fly: Fly? = null
+    val hotel: Hotel? = null
+    val status: BusinessTripStatus? =null
+}
+
+class Fly: Serializable {
+    val number: String? = null
+    val fromAirport: String? = null
+    val toAirport: String? = null
+    val flyDate: Date? = null
+}
+
+class Hotel: Serializable {
+    val name: String?  = null
+    val address: String? = null
+    val fromDate: String? = null
+    val toDate: String? = null
+}
+
+enum class BusinessTripStatus {
+   NEW,REVIEW,DONE,CANCEL
+}*/
+
+
--- src/main/kotlin/bpmn2/storm/backend/persistence/DiagramRepository.kt
+++
@@ -1,6 +0,0 @@
-package bpmn2.storm.backend.persistence
-
-import bpmn2.storm.backend.modelве.Diagram
-import org.springframework.data.repository.CrudRepository
-
-interface DiagramRepository : CrudRepository<Diagram, String>
